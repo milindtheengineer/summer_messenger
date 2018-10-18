f5f84f58ccb978493040ae0d080fa4d5895d0806
milindtheengineer <milindjuttiga@gmail.com>
2018-10-18 23:05:03 +0530

    Updated the messenging feature

--- football/scraper.go
+++ football/scraper.go
@@ -1,6 +1,7 @@
 package football
 
 import (
+	"bytes"
 	"fmt"
 	"strings"
 
@@ -15,14 +16,15 @@ func standardizeSpaces(s string) string {
 func SendMatches(date string) string {
 	url := fmt.Sprintf("https://www.goal.com/en-in/fixtures/%s/", date)
 	c := colly.NewCollector()
-	var sb strings.Builder
+	var buffer bytes.Buffer
+
 	// Find and visit all links
 	c.OnHTML("div.match-row", func(e *colly.HTMLElement) {
 		// Extract the link from the anchor HTML element
-		sb.WriteString(e.Text)
-		sb.WriteString("\n")
+		buffer.WriteString(e.Text)
+		buffer.WriteString("\n")
 	})
 
 	c.Visit(url)
-	return sb.String()
+	return buffer.String()
 }
